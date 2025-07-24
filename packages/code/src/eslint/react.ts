27d087b07252aa0e2f868168e3ebdde294dcce46
BlueOrgreen <1936341390@qq.com>
2025-07-24 18:35:22 +0800

    feat: @fan-scripts/code@1.0.6

--- packages/code/src/eslint/react.ts
+++ packages/code/src/eslint/react.ts
@@ -10,9 +10,37 @@ const config = {
       version: 'detect',
     },
   },
+  plugins: ['import', 'unused-imports'],
   rules: {
     indent: 'off',
     '@typescript-eslint/indent': [ 'error', 2 ],
+    // 模块导入顺序规则
+    'import/order': [
+        1,
+        {
+            pathGroups: [
+                {
+                    pattern: '@/**',
+                    group: 'external',
+                    position: 'after',
+                },
+            ],
+            alphabetize: { order: 'asc', caseInsensitive: false },
+            'newlines-between': 'always-and-inside-groups',
+            warnOnUnassignedImports: true,
+        },
+    ],
+    // 自动删除未使用的导入
+    // https://github.com/sweepline/eslint-plugin-unused-imports
+    'unused-imports/no-unused-imports': 1,
+    'unused-imports/no-unused-vars': [
+        'error',
+        {
+            vars: 'all',
+            args: 'none',
+            ignoreRestSiblings: true,
+        },
+    ],
   },
 };
 
